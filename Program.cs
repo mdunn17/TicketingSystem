2c1b048a231da25c8e4db60e3c4d79ce505abccb
mdunn17 <mdunn17@my.wctc.edu>
2021-03-27 20:13:48 -0500

    Added ability to search for ticket based on status, priority, or submitter

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using NLog.Web;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace TicketingSystem
 {
@@ -28,6 +29,7 @@ namespace TicketingSystem
                 Console.WriteLine("What would you like to do?");
                 Console.WriteLine("1) Read data from ticket file.");
                 Console.WriteLine("2) Create a new ticket.");
+                Console.WriteLine("3) Search for a specific ticket.");
                 Console.WriteLine("Enter any other key to exit.");
                 // input response
                 choice = Console.ReadLine();
@@ -176,8 +178,92 @@ namespace TicketingSystem
                         taskFile.AddTask(task);
                     }
 
+                }else if (choice == "3")
+                {
+                    Console.WriteLine("Do you wish to search by\n1) Status\n2) Priority\n3) Submitter");
+                    string searchType = Console.ReadLine();
+                    if (searchType == "1")
+                    {
+                        Console.WriteLine("Enter the status you wish to search for:");
+                        string input = Console.ReadLine();
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        var searchStatus1 = defectFile.Defects.Where(d => d.status.Contains(input));
+                        Console.WriteLine($"There are {searchStatus1.Count()} defects with a matching status:");
+                        foreach(Defect t in searchStatus1)
+                        {
+                            Console.WriteLine($"{t.ticketId}, {t.summary}, {t.status}, {t.priority}, {t.submitter}, {t.assigned}, {t.watching}, {t.severity}");
+                        }
+
+                        var searchStatus2 = enhancementFile.Enhancements.Where(e => e.status.Contains(input));
+                        Console.WriteLine($"There are {searchStatus2.Count()} enhancements with a matching status:");
+                        foreach(Enhancement t in searchStatus2)
+                        {
+                            Console.WriteLine($"{t.ticketId}, {t.summary}, {t.status}, {t.priority}, {t.submitter}, {t.assigned}, {t.watching}, {t.software}, {t.cost}, {t.reason}, {t.estimate}");
+                        }
+
+                        var searchStatus3 = taskFile.Task.Where(t => t.status.Contains(input));
+                        Console.WriteLine($"There are {searchStatus3.Count()} tasks with a matching status:");
+                        foreach(Task t in searchStatus3)
+                        {
+                            Console.WriteLine($"{t.ticketId}, {t.summary}, {t.status}, {t.priority}, {t.submitter}, {t.assigned}, {t.watching}, {t.projectName}, {t.dueDate}");
+                        }
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }else if (searchType == "2")
+                    {
+                        Console.WriteLine("Enter the priority you wish to search for:");
+                        string input = Console.ReadLine();
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        var searchPriority1 = defectFile.Defects.Where(d => d.priority.Contains(input));
+                        Console.WriteLine($"There are {searchPriority1.Count()} defects with a matching priority:");
+                        foreach(Defect t in searchPriority1)
+                        {
+                            Console.WriteLine($"{t.ticketId}, {t.summary}, {t.status}, {t.priority}, {t.submitter}, {t.assigned}, {t.watching}, {t.severity}");
+                        }
+
+                        var searchPriority2 = enhancementFile.Enhancements.Where(e => e.priority.Contains(input));
+                        Console.WriteLine($"There are {searchPriority2.Count()} enhancements with a matching priority:");
+                        foreach(Enhancement t in searchPriority2)
+                        {
+                            Console.WriteLine($"{t.ticketId}, {t.summary}, {t.status}, {t.priority}, {t.submitter}, {t.assigned}, {t.watching}, {t.software}, {t.cost}, {t.reason}, {t.estimate}");
+                        }
+
+                        var searchPriority3 = taskFile.Task.Where(t => t.priority.Contains(input));
+                        Console.WriteLine($"There are {searchPriority3.Count()} tasks with a matching priority:");
+                        foreach(Task t in searchPriority3)
+                        {
+                            Console.WriteLine($"{t.ticketId}, {t.summary}, {t.status}, {t.priority}, {t.submitter}, {t.assigned}, {t.watching}, {t.projectName}, {t.dueDate}");
+                        }
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }else if (searchType == "3")
+                    {
+                        Console.WriteLine("Enter the submitter you wish to search for:");
+                        string input = Console.ReadLine();
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        var searchSubmitter1 = defectFile.Defects.Where(d => d.submitter.Contains(input));
+                        Console.WriteLine($"There are {searchSubmitter1.Count()} defects with a matching submitter:");
+                        foreach(Defect t in searchSubmitter1)
+                        {
+                            Console.WriteLine($"{t.ticketId}, {t.summary}, {t.status}, {t.priority}, {t.submitter}, {t.assigned}, {t.watching}, {t.severity}");
+                        }
+
+                        var searchSubmitter2 = enhancementFile.Enhancements.Where(e => e.submitter.Contains(input));
+                        Console.WriteLine($"There are {searchSubmitter2.Count()} enhancements with a matching submitter:");
+                        foreach(Enhancement t in searchSubmitter2)
+                        {
+                            Console.WriteLine($"{t.ticketId}, {t.summary}, {t.status}, {t.priority}, {t.submitter}, {t.assigned}, {t.watching}, {t.software}, {t.cost}, {t.reason}, {t.estimate}");
+                        }
+
+                        var searchSubmitter3 = taskFile.Task.Where(t => t.submitter.Contains(input));
+                        Console.WriteLine($"There are {searchSubmitter3.Count()} tasks with a matching submitter:");
+                        foreach(Task t in searchSubmitter3)
+                        {
+                            Console.WriteLine($"{t.ticketId}, {t.summary}, {t.status}, {t.priority}, {t.submitter}, {t.assigned}, {t.watching}, {t.projectName}, {t.dueDate}");
+                        }
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    
                 }
-            } while (choice == "1" || choice == "2");
+            } while (choice == "1" || choice == "2" || choice == "3");
         }
     }
 }
